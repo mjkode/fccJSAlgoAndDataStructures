05777b1d50670230c553c7e5538d1c3917600a4f
Michael Kiger <MJK@wpsitework.com>
2019-09-22 23:00:18 -0400

    second

--- booWho.js
+++ booWho.js
@@ -1,6 +1,6 @@
 function booWho(bool) {
   // What is the new fad diet for ghost developers? The Boolean.
-  console.log(typeof bool);
+  console.log(typeof bool); 
   console.log(typeof bool === 'boolean');
   return typeof bool === 'boolean';
   
